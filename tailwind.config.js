63aa9db60f09593422ec31b86ca9b946688ef528
Saad-Ahmed-Khanzada <saadkhanzada1@gmail.com>
2024-11-19 17:15:23 +0500

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",  // Include the main HTML file
-    "./src/**/*.{js,ts,jsx,tsx}", // Include all JavaScript/TypeScript files in the src folder
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      // Customize your theme here
-      colors: {
-        primary: "#1D4ED8", // Example custom color
-        secondary: "#9333EA",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"], // Example custom font
-      },
-    },
+    extend: {},
   },
-  plugins: [
-
-  ],
-};
+  plugins: [],
+}
